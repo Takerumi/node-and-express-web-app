22b66dd2049cab846d85bc0c4f63074b7a015e7f
Borman <mrgabrielripper@gmail.com>
2022-02-23 17:08:29 +0600

    Starting work on site

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars')
+        .create({ defaultLayout: 'main' });
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.use(function(req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+    console.log(`Express запущен на http://localhost:${app.get('port')}; Нажмите Ctrl+C для завершения.`);
+});
