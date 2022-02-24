50629b3b89ac801b6b52b9911b4f9c7ce5c92095
Borman <mrgabrielripper@gmail.com>
2022-02-24 21:29:34 +0600

    Work on QA page testing

--- app.js
+++ app.js
@@ -11,12 +11,28 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    res.render('about', { fortune: fortune.getFortune() });
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js' 
+    });
+});
+
+app.get('/products/tyrannofex', function(req, res) {
+    res.render('products/tyrannofex');
+});
+
+app.get('/products/request-item-price', function(req, res) {
+    res.render('products/request-item-price');
 });
 
 app.use(function(req, res, next) {
---
+++ public/qa/tests-global.js
@@ -0,0 +1,5 @@
+suite('Global Tests', function() {
+    test('У данной страницы допустимый заголовок', function() {
+        assert(document.title && document.title.match(/\S/) && document.title.toUpperCase() !== 'TODO');
+    });
+});
