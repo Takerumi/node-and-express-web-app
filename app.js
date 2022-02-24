b5e7f435db590819eed2a0875360728146923106
Borman <mrgabrielripper@gmail.com>
2022-02-24 20:40:28 +0600

    Add 'fortune cookie' functionality into module.

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars')
         .create({ defaultLayout: 'main' });
+const fortune = require('./lib/fortune');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    res.render('about');
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 app.use(function(req, res, next) {
