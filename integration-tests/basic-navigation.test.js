3b265ef6860eb18ab8e9bb36a39ee389a2cd65e9
Borman <mrgabrielripper@gmail.com>
2022-02-27 15:31:59 +0600

    Work on unit and integration tests

---
+++ integration-tests/basic-navigation.test.js
@@ -0,0 +1,28 @@
+const portfinder = require('portfinder')
+const puppeteer = require('puppeteer')
+
+const app = require('../app')
+
+let server = null
+let port = null
+
+beforeEach(async () => {
+    port = await portfinder.getPortPromise()
+    server = app.listen(port)
+})
+
+afterEach(() => {
+    server.close()
+})
+
+test('Домашняя страница ссылается на страницу "О нас"', async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(`http://localhost:${port}`)
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('[data-test-id="about"]')
+    ])
+    expect(page.url()).toBe(`http://localhost:${port}/about`)
+    await browser.close()
+})
