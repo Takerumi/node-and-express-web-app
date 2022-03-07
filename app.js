c0313cba9c347613a61def4f929acceb12134558
Borman <mrgabrielripper@gmail.com>
2022-03-07 12:29:16 +0600

    Add comment & helpers to handlebars engine

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
-
 const multiparty = require('multiparty')
 
 const handlers = require('./lib/handlers')
@@ -11,7 +10,14 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.engine('handlebars', engine({
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options) {
+            if(!this._sections) this._sections = {}
+            this._sections[name] = options.fn(this)
+            return null
+        }
+    }
 }))
 
 app.set('view engine', 'handlebars');
@@ -24,12 +30,21 @@ app.get('/', handlers.home);
 
 app.get('/about', handlers.about);
 
-app.get('/newsletter', handlers.newsletter)
+//обработчики отправки формы в браузере
 app.get('/newsletter-signup', handlers.api.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
-app.post('/contest/miniatyres-compendium/:year/:month', (req, res) => {
+//обработчики отправки формы JSON/fetch
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
+//фотоконкурс покраса миниатюр
+app.get('/contest/miniatyres-compendium', handlers.miniatyresCompendiumContest)
+
+
+app.get('/contest/miniatyres-compendium-thank-you', handlers.miniatyresCompendiumContestProcessThankYou)
+app.post('/contest/miniatyres-compendium-contest/:year/:month', (req, res) => {
     const form = new multiparty.Form()
     form.parse(req, (err, fields, files) => {
         if(err) return res.status(500).send({ error: err.message })
@@ -39,7 +54,6 @@ app.post('/contest/miniatyres-compendium/:year/:month', (req, res) => {
 
 // Пользовательская страница 404
 app.use(handlers.notFound);
-
 // Пользовательская страница 500
 app.use(handlers.serverError);
 
