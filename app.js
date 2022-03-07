83b85e1dba0b7ec18ed7194c44d15c0b12874904
Borman <mrgabrielripper@gmail.com>
2022-03-07 15:55:23 +0600

    working with middleware to display notifications using sessions

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
-const multiparty = require('multiparty')
+// const multiparty = require('multiparty')
 const cookieParser = require('cookie-parser')
-const handlers = require('./lib/handlers')
+const expressSession = require('express-session')
 const credentials = require('./credentials')
+const handlers = require('./lib/handlers')
+const flashMiddleware = require('./lib/middleware/flash')
 
 const app = express()
-
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-app.use(cookieParser(credentials.cookieSecret))
 
 app.engine('handlebars', engine({
     defaultLayout: 'main',
@@ -24,9 +22,20 @@ app.engine('handlebars', engine({
 
 app.set('view engine', 'handlebars');
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cookieParser(credentials.cookieSecret))
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret
+}))
+
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(flashMiddleware)
 
 app.get('/', handlers.home);
 
@@ -38,21 +47,21 @@ app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
 //обработчики отправки формы JSON/fetch
-app.get('/newsletter', handlers.newsletter)
-app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+// app.get('/newsletter', handlers.newsletter)
+// app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
 
 //фотоконкурс покраса миниатюр
-app.get('/contest/miniatyres-compendium', handlers.miniatyresCompendiumContest)
+// app.get('/contest/miniatyres-compendium', handlers.miniatyresCompendiumContest)
 
 
-app.get('/contest/miniatyres-compendium-thank-you', handlers.miniatyresCompendiumContestProcessThankYou)
-app.post('/contest/miniatyres-compendium-contest/:year/:month', (req, res) => {
-    const form = new multiparty.Form()
-    form.parse(req, (err, fields, files) => {
-        if(err) return res.status(500).send({ error: err.message })
-        handlers.miniatyresCompendiumContestProcess(req, res, fields, files)
-    })
-})
+// app.get('/contest/miniatyres-compendium-thank-you', handlers.miniatyresCompendiumContestProcessThankYou)
+// app.post('/contest/miniatyres-compendium-contest/:year/:month', (req, res) => {
+//     const form = new multiparty.Form()
+//     form.parse(req, (err, fields, files) => {
+//         if(err) return res.status(500).send({ error: err.message })
+//         handlers.miniatyresCompendiumContestProcess(req, res, fields, files)
+//     })
+// })
 
 // Пользовательская страница 404
 app.use(handlers.notFound);
